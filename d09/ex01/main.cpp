2e84cb63b719991b69f784896ccb53f8f879e235
perche_r <raphael.perchec@epitech.eu>
2016-01-14 15:20:24 +0100

    ex01

---
+++ d09/ex01/main.cpp
@@ -0,0 +1,82 @@
+//
+// main.cpp for raph in /home/perche_r/piscine_cpp/piscine_cpp_d09/ex01
+// 
+// Made by Perchec Raphael
+// Login   <perche_r@epitech.net>
+// 
+// Started on  Thu Jan 14 14:21:58 2016 Perchec Raphael
+// Last update Thu Jan 14 14:56:51 2016 Perchec Raphael
+//
+
+#include <iostream>
+#include "Character.hh"
+#include "Warrior.hh"
+
+int	main()
+{
+  Character leo("leo", 42);
+  Character flo("flo", 42);
+  Warrior   johann("johann", 56);
+  Warrior   raph("raph", 56);
+
+  if (leo.getLvl() != 42 ||
+      leo.getPv() != 100 ||
+      leo.getPower() != 100 ||
+      leo.getName() != "leo" ||
+      flo.getName() != "flo")
+    {
+      std::cout << "CRITICAL ERROR ON DEFAULT VALUES" << std::endl;
+      return (-1);
+    }
+  if (johann.getLvl() != 56 ||
+      johann.getPv() != 100 ||
+      johann.getPower() != 100 ||
+      johann.getName() != "johann" ||
+      raph.getName() != "raph")
+    {
+      std::cout << "CRITICAL ERROR ON DEFAULT VALUES" << std::endl;
+      return (-1);
+    }
+  flo.TakeDamage(leo.CloseAttack());
+  leo.TakeDamage(flo.RangeAttack());
+
+  flo.Heal();
+  flo.TakeDamage(leo.RangeAttack());
+
+  leo.TakeDamage(flo.CloseAttack());
+  leo.TakeDamage(flo.CloseAttack());
+
+  leo.Heal();
+
+  flo.RestorePower();
+
+  leo.RestorePower();
+  leo.RestorePower();
+  leo.RestorePower();
+
+  std::cout << leo.getName() << " is being a boss thanks to his food!" << std::endl;
+
+  flo.TakeDamage(leo.CloseAttack() * 6);
+  if (flo.getPv() > 0)
+    std::cout << "CRITICAL ERROR : FLO IS SUPPOSED TO BE DEAD!!" << std::endl;
+  flo.TakeDamage(1);
+
+  johann.TakeDamage(leo.CloseAttack());
+  leo.TakeDamage(raph.RangeAttack());
+  johann.TakeDamage(raph.RangeAttack());
+  johann.TakeDamage(raph.CloseAttack());
+  std::cout << leo.getName() << " Life remaining : " << leo.getPv() << std::endl;
+  std::cout << leo.getName() << " Power remaining : " << leo.getPower() << std::endl;
+  std::cout << johann.getName() << " Life remaining : " << johann.getPv() << std::endl;
+  std::cout << johann.getName() << " Power remaining : " << johann.getPower() << std::endl;
+  std::cout << raph.getName() << " Life remaining : " << raph.getPv() << std::endl;
+  std::cout << raph.getName() << " Power remaining : " << raph.getPower() << std::endl;
+  johann.Heal();
+  std::cout << johann.getName() << " Life remaining : " << johann.getPv() << std::endl;
+  johann.TakeDamage(raph.CloseAttack());
+  johann.TakeDamage(raph.CloseAttack());
+  johann.TakeDamage(raph.CloseAttack());
+  raph.RestorePower();
+  johann.TakeDamage(raph.CloseAttack());
+  johann.TakeDamage(raph.CloseAttack());
+}
